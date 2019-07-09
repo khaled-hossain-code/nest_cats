3946627708532fef36d5bc459d61f580caee50b7
Khaled Hossain <md.khaled.eee@gmail.com>
2019-07-09 08:11:20 +0600

    add validation to .env

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from './config/config.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private port: String;
+
+  constructor(config: ConfigService){
+    this.port = config.get('port');
+  }
+  getHello(): any {
+    return this.port;
   }
 }
--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,18 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import * as Joi from 'joi';
+
+export interface EnvConfig {
+  [key: string]: any;
+}
 
 @Injectable()
 export class ConfigService {
-  private readonly envConfig: { [key: string]: any };
+  private readonly envConfig: EnvConfig;
 
   constructor() {
-    console.log('process.env :', process.env.PORT);
-    this.envConfig = {
-      port: process.env.PORT,
+    const config = {
+      PORT: process.env.PORT,
       isProduction: process.env.NODE_ENV === 'production',
-    }
+    };
+
+    this.envConfig = this.validateInput(config);
   }
 
   get(key: string): string {
     return this.envConfig[key];
+  }
+
+  private validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema: Joi.ObjectSchema = Joi.object({
+      NODE_ENV: Joi.string()
+        .valid(['development', 'production', 'test', 'provision'])
+        .default('development'),
+      PORT: Joi.number().default(3000),
+      isProduction: Joi.boolean().default(false),
+    });
+
+    const { error, value: validatedEnvConfig } = Joi.validate(
+      envConfig,
+      envVarsSchema,
+    );
+
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+
+    return validatedEnvConfig;
   }
 }
