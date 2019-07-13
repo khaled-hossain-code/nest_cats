0038be6d4a5f8711c5861da084bda1d6df91301d
Khaled Hossain <md.khaled.eee@gmail.com>
2019-07-13 16:17:54 +0600

    create cache interceptors

---
+++ src/common/interceptors/cache.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, of } from 'rxjs';
+
+@Injectable()
+export class CacheInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const isCached = false;
+
+    if (isCached) {
+      return of([]);
+    }
+
+    return next.handle();
+  }
+}
